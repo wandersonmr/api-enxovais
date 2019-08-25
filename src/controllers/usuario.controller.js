35d74f084ca5b87f81d209d843137ed2514f5c49
Wanderson Marques <wandersonmro@gmail.com>
2019-08-25 18:10:57 -0300

    Conexão com banco

--- src/controllers/usuario.controller.js
+++ src/controllers/usuario.controller.js
@@ -3,6 +3,16 @@
 const response = require('../../config/response');
 
 module.exports.getUsuario = async (req, res) =>{
-    
-    response.montaRetornoAPI({data: "sucesso"}, req, res);
+    try {
+
+        // Realiza a conexao no banco de URLs.
+        let conectaURLs = dataBase.getConnection();
+
+        response.montaRetornoAPI({data: "sucesso"}, req, res);
+        
+    } catch (erro) {
+        response.montaRetornoAPI({ status: 400, erro }, req, res);
+    }
+
+
 }
